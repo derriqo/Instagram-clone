f170ca36c328ad3c7a99f11d357b2bc350bb8099
derrrick william <williamderrick100@gmail.com>
2019-03-11 12:09:29 +0300

    Installed Bootstrap 3

--- album/urls.py
+++ album/urls.py
@@ -4,4 +4,5 @@ from instap import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('instap.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
