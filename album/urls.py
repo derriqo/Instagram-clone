8cd526aa002b228f3e314317ee1a3f4a1cafb209
derrrick william <williamderrick100@gmail.com>
2019-03-11 10:34:14 +0300

    Modified views.

--- album/urls.py
+++ album/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from users import views 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('instap.urls'))
+    url(r'register/',views.mwanzo,name='register')
+    url(r'',include('instap.urls')),
 ]
