b5c6e0048afbda9b93441e82a846197f57d66aa1
derrrick william <williamderrick100@gmail.com>
2019-03-11 09:57:07 +0300

    Created templates and models.

--- instap/models.py
+++ instap/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Profile(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    phone_number = models.CharField(max_length = 10,blank = True)
+# class Profile(models.Model):
+#     profile_pic = models.ImageField()
+#     username = models.OneToOneField(User, on_delete=models.CASCADE)
+#     bio = models.TextField(max_length=500, blank=True)
+#     phone_number = models.CharField(max_length = 10,blank = True)
     
-    
+class Image(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)    
