aa9490e29eff2e6d792625f24145178b1344dc82
derrrick william <williamderrick100@gmail.com>
2019-03-12 07:15:20 +0300

    Installed crispy forms.

--- instap/views.py
+++ instap/views.py
@@ -12,9 +12,10 @@ def registration(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form is valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account created for {username}!')
-            return redirect ('mwanzo')
+            return redirect ('login')
     else:
         form = UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
