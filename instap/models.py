2a9f60a5944dbbb8a61e0b32111798db1000b20e
derrrick william <williamderrick100@gmail.com>
2019-03-12 09:46:40 +0300

    Installed Pillow.

--- instap/models.py
+++ instap/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your models here.
-# class Profile(models.Model):
-#     profile_pic = models.ImageField()
-#     username = models.OneToOneField(User, on_delete=models.CASCADE)
-#     bio = models.TextField(max_length=500, blank=True)
-#     phone_number = models.CharField(max_length = 10,blank = True)
+class Profile(models.Model):
+    profile_pic = models.ImageField()
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    phone_number = models.CharField(max_length = 10,blank = True)
     
 class Image(models.Model):
     title = models.CharField(max_length=100)
