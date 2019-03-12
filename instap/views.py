ab148cb0c07a57daf1290f09c9cf90b9630ca72a
derrrick william <williamderrick100@gmail.com>
2019-03-12 10:24:56 +0300

    View functions added.

--- instap/views.py
+++ instap/views.py
@@ -13,10 +13,11 @@ def registration(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form is valid():
-            new_user = form.save(commit=False)
-            new_user.username = form.cleaned_data.get('username')
-            new_user.raw_pass = form.clean_data.get('password1')
-            new_user.save()
+            form.save()
+            # new_user = form.save(commit=False)
+            # new_user.username = form.cleaned_data.get('username')
+            # new_user.raw_pass = form.clean_data.get('password1')
+            # new_user.save()
             # authenticate(username=username,password=raw_pass)
             messages.success(request,f'Account created for {username}!')
             return redirect('login')
