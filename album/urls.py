3a9e4d4e67042f8e6aad22f0df0f13e9054fad60
derrrick william <williamderrick100@gmail.com>
2019-03-12 06:53:32 +0300

    Added new templates.

--- album/urls.py
+++ album/urls.py
@@ -6,7 +6,7 @@ from instap import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('register/',views.registration, name ='register'),
-    url('login/',auth_views.LoginView.as_view(), name ='login'),
-     url('logout/',auth_views.LogoutView.as_view(), name ='logout'),
+    url('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name ='login'),
+     url('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name ='logout'),
     url(r'',include('instap.urls')),
 ]
